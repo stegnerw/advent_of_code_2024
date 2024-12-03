5fcc7f17219d74b11f5323e4302865e07919f8ac
Wayne Stegner <wayne.stegner@protonmail.com>
2024-12-03 01:08:15 -0500

    Add day 3 part 2

--- day_03/MullItOver.jl
+++ day_03/MullItOver.jl
@@ -20,3 +20,40 @@ for str in mul_strings
 end
 
 println("muls sums: ", mul_sum)
+
+##########
+# Part 2 #
+##########
+
+# This doesn't work because regex is greedy and prunes too much
+#dont_do_reg = r"don't\(\).*do\(\)"
+#dont_do_strings = [m.match for m = eachmatch(dont_do_reg, raw_input)]
+#println(dont_do_strings)
+#filtered_input = replace(raw_input, dont_do_reg => "")
+
+filtered_input = raw_input
+while true
+  global filtered_input
+  dont_idx = findfirst("don't()", filtered_input)
+  if isnothing(dont_idx)
+    break
+  end
+  # Have to search after the first don't() command
+  do_idx = findfirst("do()", filtered_input[dont_idx[end]+1:end])
+  chop_start = dont_idx[1]-1
+  # Index relative to the don't() command
+  chop_end = do_idx[end]+1 + dont_idx[end]
+  filtered_input = filtered_input[1:chop_start] * filtered_input[chop_end:end]
+end
+
+mul_strings = [m.match for m = eachmatch(mul_reg, filtered_input)]
+
+mul_sum = 0
+
+for str in mul_strings
+  substr = str[5:end-1]
+  nums = [parse(Int, x) for x in split(substr, ",")]
+  global mul_sum += nums[1] * nums[2]
+end
+
+println("filtered muls sums: ", mul_sum)
