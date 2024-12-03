778620ef93f54699d3749278fc065d8b7cbf5529
Wayne Stegner <wayne.stegner@protonmail.com>
2024-12-02 21:59:54 -0500

    Add day 1 part 2 solution

--- day_01/HistorianHysteria.jl
+++ day_01/HistorianHysteria.jl
@@ -1,19 +1,48 @@
 #!/usr/bin/env julia
 using DelimitedFiles
 
+##############
+# Input Prep #
+##############
+
 # Read delimited file
-raw_input = readdlm("./input.txt", ' ', Int, '\n')
+# Using @__DIR__ so it doesn't matter where cwd is
+raw_input = readdlm(string(@__DIR__, "/input.txt"), ' ', Int, '\n')
+
+# List slicing (1-indexed, bleh)
+list1 = raw_input[:,1]
+list2 = raw_input[:,2]
+
+##########
+# Part 1 #
+##########
 
 # Nice built-in sort function
-list1 = sort(raw_input[:,1])
-list2 = sort(raw_input[:,2])
+list1_sorted = sort(list1)
+list2_sorted = sort(list2)
 
 # You can operate on lists as vectors
-diff = list1 - list2
+diff = list1_sorted - list2_sorted
 
 # Execute a func over a list with broadcast
 abs_diff = broadcast(abs, diff)
 
 # Sum them up
 diff_sum = sum(abs_diff)
-print("Total difference: ", diff_sum)
+println("Total difference: ", diff_sum)
+
+##########
+# Part 2 #
+##########
+
+similarity = 0
+
+# I was going to pre-map how many times each number occurs but list1 is all
+# unique values so it's easier to just iterate
+# y->y==x is an anonymous function; count counts how many elements of list2
+# evaluate to true in that function
+for x in list1
+  global similarity += x * count(y->y==x, list2)
+end
+
+println("Similarity: ", similarity)
