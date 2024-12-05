33330d42a0212841a25434704eca789b6dee0837
Wayne Stegner <wayne.stegner@protonmail.com>
2024-12-05 01:56:00 -0500

    Add day 5 part 2

--- day_05/PrintQueue.jl
+++ day_05/PrintQueue.jl
@@ -28,6 +28,7 @@ end
 ##########
 
 valid_prints = Vector{Vector{Int}}()
+invalid_prints = Vector{Vector{Int}}()
 for print in prints
   print_set = Set{Int}()
   print_valid = true
@@ -45,19 +46,56 @@ for print in prints
   end
   if print_valid
     push!(valid_prints, print)
+  else
+    push!(invalid_prints, print)
   end
 end
 
-middle_page_sum = 0
+valid_middle_page_sum = 0
 for print in valid_prints
   # The round() function follows proper sig-fig rules
   # 8.5 rounds to 8, and 9.5 rounds to 10
   middle_page_idx = trunc(Int, ceil(length(print)/2))
-  global middle_page_sum += print[middle_page_idx]
+  global valid_middle_page_sum += print[middle_page_idx]
 end
 
-println("Middle page sum: $(middle_page_sum)")
+println("Valid middle page sum: $(valid_middle_page_sum)")
 
 ##########
 # Part 2 #
 ##########
+
+for print in invalid_prints
+  changed = true
+  while changed
+    changed = false
+    for test_idx in reverse(1:length(print))
+      test_page = print[test_idx]
+      insert_idx = nothing
+      if !haskey(rules, test_page)
+        continue
+      end
+      for probe_idx in reverse(1:test_idx-1)
+        if in(print[probe_idx], rules[test_page])
+          insert_idx = probe_idx
+        end
+      end
+      if !isnothing(insert_idx)
+        deleteat!(print, test_idx)
+        insert!(print, insert_idx, test_page)
+        changed = true
+      end
+    end
+  end
+end
+
+invalid_middle_page_sum = 0
+for print in invalid_prints
+  # The round() function follows proper sig-fig rules
+  # 8.5 rounds to 8, and 9.5 rounds to 10
+  middle_page_idx = trunc(Int, ceil(length(print)/2))
+  global invalid_middle_page_sum += print[middle_page_idx]
+end
+
+println("Valid middle page sum: $(invalid_middle_page_sum)")
+
